0e7e492e344cb644cbe9ec7d303bf7170585730e
Khan Mohammad R <codemascot@hotmail.com>
2021-03-01 22:19:52 +0600

    Implemented a silly frontend data as I'm not good at HTML/CSS

---
+++ inc/Admin/Views/html-render-frontend.php
@@ -0,0 +1,9 @@
+<div class="card">
+    <div class="card-feature">
+        <div class="card-top">
+            <h3 class="card-title">
+				<?php echo $resp['hits']['hits'][0]['_source']['name']; ?>
+			</h3>
+        </div>
+    </div>
+</div>
