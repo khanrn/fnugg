d5227ec7348352311b24ac14180b3ccaf3bf6965
Khan Mohammad R <codemascot@hotmail.com>
2021-03-11 19:54:44 +0600

    Some PHPCS fix

--- tests/phpunit/BlockTest.php
+++ tests/phpunit/BlockTest.php
@@ -3,56 +3,56 @@ declare(strict_types=1);
 
 namespace Fnugg\Tests;
 
-use \WP_Mock\Tools\TestCase;
 use \Fnugg\Block\Block;
+use \WP_Mock\Tools\TestCase;
 
 class BlockTest extends TestCase
 {
-	private $block;
-	private $atts = [
-		'name'         => 'Test Name',
-		'sourceFields' => 'name,description',
-	];
+    private $block;
+    private $atts = [
+        'name'         => 'Test Name',
+        'sourceFields' => 'name,description',
+    ];
 
-	public function setUp(): void
-	{
-		parent::setUp();
-		$this->block = new Block([
-			'dir'  => realpath(__DIR__ . '/../../'),
-			'file' => realpath(__DIR__ . '/../../') . '/fnugg.php',
-		]);
+    public function setUp() : void
+    {
+        parent::setUp();
+        $this->block = new Block([
+            'dir'  => realpath(__DIR__ . '/../../'),
+            'file' => realpath(__DIR__ . '/../../') . '/fnugg.php',
+        ]);
 
-		\WP_Mock::userFunction(
+        \WP_Mock::userFunction(
             'is_admin',
             [
                 'return' => true,
             ]
         );
-	}
+    }
 
-	public function tearDown() : void
-	{
-		parent::tearDown();
-	}
+    public function tearDown() : void
+    {
+        parent::tearDown();
+    }
 
-	public function test_init_block_added_to_action_hook_init() : void
-	{
-		\WP_Mock::expectActionAdded( 'init', [ $this->block, 'init_block' ] );
-		$this->block->init();
-		$this->assertActionsCalled();
-	}
+    public function test_init_block_added_to_action_hook_init() : void
+    {
+        \WP_Mock::expectActionAdded('init', [$this->block, 'init_block']);
+        $this->block->init();
+        $this->assertActionsCalled();
+    }
 
-	public function test_block_editor_assets_added_to_action_hook_enqueue_block_editor_assets() : void
-	{
-		\WP_Mock::expectActionAdded( 'enqueue_block_editor_assets', [ $this->block, 'block_editor_assets' ] );
-		$this->block->init();
-		$this->assertActionsCalled();
-	}
+    public function test_block_editor_assets_added_to_action_hook_enqueue_block_editor_assets() : void
+    {
+        \WP_Mock::expectActionAdded('enqueue_block_editor_assets', [$this->block, 'block_editor_assets']);
+        $this->block->init();
+        $this->assertActionsCalled();
+    }
 
-	public function test_block_assets_added_to_action_hook_enqueue_block_assets() : void
-	{
-		\WP_Mock::expectActionAdded( 'enqueue_block_assets', [ $this->block, 'block_assets' ] );
-		$this->block->init();
-		$this->assertActionsCalled();
-	}
+    public function test_block_assets_added_to_action_hook_enqueue_block_assets() : void
+    {
+        \WP_Mock::expectActionAdded('enqueue_block_assets', [$this->block, 'block_assets']);
+        $this->block->init();
+        $this->assertActionsCalled();
+    }
 }
--- tests/phpunit/FetchTest.php
+++ tests/phpunit/FetchTest.php
@@ -3,81 +3,79 @@ declare(strict_types=1);
 
 namespace Fnugg\Tests;
 
-use \WP_Mock\Tools\TestCase;
 use \Fnugg\Data;
 use \Fnugg\Shared\Helpers;
-use \Fnugg\Block\Block;
+use \WP_Mock\Tools\TestCase;
 
 class FetchTest extends TestCase
 {
-	private $body = [
-		'total'        => 14,
-		'more_results' => true,
-		'result'       => [
-			0 => [
-				'name'      => 'Feforbakken',
-				'site_path' => '/feforbakken/',
-			],
-			1 => [
-				'name'      => 'Myrkdalen Fjellheiser',
-				'site_path' => '/myrkdalen/',
-			],
-			2 => [
-				'name'      => 'Fagerfjell skisenter',
-				'site_path' => '/fagerfjell/',
-			],
-			3 => [
-				'name'      => 'Høgevarde Fjellpark',
-				'site_path' => '/hogevarde/',
-			],
-			4 => [
-				'name'      => 'Sulitjelma Fjellandsby',
-				'site_path' => '/sulitjelma/',
-			],
-			5 => [
-				'name'      => 'Furedalen Alpin',
-				'site_path' => '/furedalen/',
-			],
-			6 => [
-				'name'      => 'Fjellhaugen Skisenter',
-				'site_path' => '/fjellhaugen/',
-			],
-			7 => [
-				'name'      => 'Sunnmørsalpane Skiarena Fjellsætra',
-				'site_path' => '/sunnmorsalpene/',
-			],
-			8 => [
-				'name'      => 'Fjellsyn skisenter',
-				'site_path' => '/fjellsyn/',
-			],
-			9 => [
-				'name'      => 'Fulufjellet Alpinsenter',
-				'site_path' => '/fulufjellet/',
-			],
-		],
-	];
+    private $body = [
+        'total'        => 14,
+        'more_results' => true,
+        'result'       => [
+            0 => [
+                'name'      => 'Feforbakken',
+                'site_path' => '/feforbakken/',
+            ],
+            1 => [
+                'name'      => 'Myrkdalen Fjellheiser',
+                'site_path' => '/myrkdalen/',
+            ],
+            2 => [
+                'name'      => 'Fagerfjell skisenter',
+                'site_path' => '/fagerfjell/',
+            ],
+            3 => [
+                'name'      => 'Høgevarde Fjellpark',
+                'site_path' => '/hogevarde/',
+            ],
+            4 => [
+                'name'      => 'Sulitjelma Fjellandsby',
+                'site_path' => '/sulitjelma/',
+            ],
+            5 => [
+                'name'      => 'Furedalen Alpin',
+                'site_path' => '/furedalen/',
+            ],
+            6 => [
+                'name'      => 'Fjellhaugen Skisenter',
+                'site_path' => '/fjellhaugen/',
+            ],
+            7 => [
+                'name'      => 'Sunnmørsalpane Skiarena Fjellsætra',
+                'site_path' => '/sunnmorsalpene/',
+            ],
+            8 => [
+                'name'      => 'Fjellsyn skisenter',
+                'site_path' => '/fjellsyn/',
+            ],
+            9 => [
+                'name'      => 'Fulufjellet Alpinsenter',
+                'site_path' => '/fulufjellet/',
+            ],
+        ],
+    ];
 
-	private $url = 'https://api.fnugg.no/';
-	private $uri = '/suggest/autocomplete/';
+    private $url = 'https://api.fnugg.no/';
+    private $uri = '/suggest/autocomplete/';
 
-	private $q = [
-		'name'         => 'Test Name',
-		'sourceFields' => 'name,description',
-	];
+    private $q = [
+        'name'         => 'Test Name',
+        'sourceFields' => 'name,description',
+    ];
 
+    private $fetch;
 
-	private $fetch;
+    public function __construct()
+    {
+        parent::__construct();
+        $this->fetch = new Data\Fetch($this->url);
+    }
 
-	public function __construct()
-	{
-		parent::__construct();
-		$this->fetch = new Data\Fetch($this->url);
-	}
-
-	public function setUp(): void
-	{
-		parent::setUp();
-		\WP_Mock::userFunction(
+    public function setUp() : void
+    {
+        parent::setUp();
+        \WP_Mock::userFunction(
             'wp_remote_get',
             [
                 'return' => [
@@ -97,26 +95,26 @@ class FetchTest extends TestCase
             ]
         );
 
-		\WP_Mock::userFunction(
-			'add_query_arg',
-			[
-				'return' => $this->url . $this->uri . str_replace('%2C', ',', http_build_query($this->q)),
-			]
-		);
-	}
+        \WP_Mock::userFunction(
+            'add_query_arg',
+            [
+                'return' => $this->url . $this->uri . str_replace('%2C', ',', http_build_query($this->q)),
+            ]
+        );
+    }
 
-	public function tearDown() : void
-	{
-		parent::tearDown();
-	}
-
-	public function test_get_remote_json_method() : void
+    public function tearDown() : void
     {
-		$r = Helpers::get_remote_json($this->url);
-        $this->assertTrue(is_array($r));
-	}
+        parent::tearDown();
+    }
 
-	public function test_trans_id_string_length_is_64() : void
+    public function test_get_remote_json_method() : void
+    {
+        $r = Helpers::get_remote_json($this->url);
+        $this->assertTrue(is_array($r));
+    }
+
+    public function test_trans_id_string_length_is_64() : void
     {
         $h = Helpers::trans_id($this->q, 'test');
 
@@ -127,15 +125,15 @@ class FetchTest extends TestCase
         $this->assertTrue($s);
     }
 
-	public function test_autocompelte_api_call() : void
-	{
-		$r = $this->fetch->autocomplete($this->q);
-		$this->assertNotNull($r);
-		$this->assertTrue(is_array($r));
-		$this->assertCount(3, $r);
-		$this->assertTrue($r['more_results']);
-		$this->assertEquals(14, $r['total']);
-		$this->assertCount(10, $r['result']);
-		$this->assertNotNull($r['result'][5]['name']);
-	}
+    public function test_autocompelte_api_call() : void
+    {
+        $r = $this->fetch->autocomplete($this->q);
+        $this->assertNotNull($r);
+        $this->assertTrue(is_array($r));
+        $this->assertCount(3, $r);
+        $this->assertTrue($r['more_results']);
+        $this->assertEquals(14, $r['total']);
+        $this->assertCount(10, $r['result']);
+        $this->assertNotNull($r['result'][5]['name']);
+    }
 }
